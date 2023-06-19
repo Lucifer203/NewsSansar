69da6a4ba7aafcafb69a8564ee2c1485555549c1
Adarsh Ghimire <93777888+Lucifer203@users.noreply.github.com>
2023-06-19 22:09:35 +0545

    Previous and next function of going to next page has been added

--- src/components/News.js
+++ src/components/News.js
@@ -79,8 +79,52 @@ export default class News extends Component {
     this.state = {
       articles: this.articles,
       loading: false,
+      page: 1,
     };
   }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=5dfeff9303ba42459344afbe6c65ed73&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5dfeff9303ba42459344afbe6c65ed73&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5dfeff9303ba42459344afbe6c65ed73&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
 
   render() {
     return (
@@ -92,14 +136,33 @@ export default class News extends Component {
             return (
               <div className="col-md-4" key={index}>
                 <NewsItem
-                  title={article.title.slice(0, 45)}
-                  description={article.description.slice(0, 88)}
+                  title={article.title ? article.title.slice(0, 45) : ""}
+                  description={
+                    article.description ? article.description.slice(0, 88) : ""
+                  }
                   imageUrl={article.urlToImage}
                   newsUrl={article.url}
                 />
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePreviousClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            &rarr; Next
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,15 +6,19 @@ export default class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://www.hindustantimes.com/ht-img/img/2023/06/17/1600x900/CRICKET-TEST-ENG-AUS--50_1687004449412_1687004508458.JPG"
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-primary"
-            >
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
